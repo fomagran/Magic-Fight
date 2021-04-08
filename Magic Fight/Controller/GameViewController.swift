1be18c39f8567ff2aa3c6e59da02bf1ab378c389
Fomagran <fomagran6@naver.com>
2021-04-08 11:24:36 +0900

    타이머 구현

--- Magic Fight/Controller/GameViewController.swift
+++ Magic Fight/Controller/GameViewController.swift
@@ -23,9 +23,54 @@ class GameViewController: UIViewController {
     @IBOutlet weak var logLabel: UIImageView!
     @IBOutlet weak var timerLabel: UILabel!
     
+    var timer = Timer()
+    var (minutes,seconds) = (1,0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-  
+        start()
+    }
+    
+   func reset(_ sender: Any) {
+        timer.invalidate()
+        (minutes,seconds) = (1,0)
+        timerLabel.text = "1:00"
+    }
+    
+    func start() {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.keepTimer), userInfo: nil, repeats: true)
+
+    }
+    
+    @objc private func stopTimer() {
+        timer.invalidate()
+    }
+    
+    //처음상태로 세팅
+    private func setInitialState() {
+        timer.invalidate()
+        (minutes,seconds) = (1,0)
+        timerLabel.text = "01:00"
+    }
+    
+    @objc private func keepTimer(){
+        if seconds == 0 {
+            if minutes == 1 {
+            minutes -= 1
+            seconds = 59
+            }else{
+                timer.invalidate()
+            }
+        }else{
+            seconds -= 1
+        }
+
+        
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+      
+        timerLabel.text = "\(minutesString):\(secondsString)"
     }
 }
