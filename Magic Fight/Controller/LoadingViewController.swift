cdd68ab396d5cc102f3c9aa5d960b001bd0e6593
Fomagran <fomagran6@naver.com>
2021-10-22 22:43:05 +0900

    [add] 닉네임 중복체크하기

--- Magic Fight/Controller/LoadingViewController.swift
+++ Magic Fight/Controller/LoadingViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class LoadingViewController: UIViewController {
     
@@ -29,20 +30,37 @@ class LoadingViewController: UIViewController {
         }
     }
     
-    func showAlert() {
-        let alert = UIAlertController(title: title, message: "닉네임을 정해주세요!", preferredStyle: .alert)
+    func showAlert(title:String) {
+        let alert = UIAlertController(title: title, message:title, preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default) { (_) in
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
+    func checkOverlap(completion:@escaping(Bool)->Void) {
+        Firestore.firestore().collection("User").whereField("nickName",isEqualTo:nickNameTF.text ?? "").getDocuments { snapshot, error in
+            guard let snapshot = snapshot else { return completion(false) }
+            if !snapshot.documents.isEmpty {
+                completion(true)
+            }else {
+                completion(false)
+            }
+        }
+    }
+    
     
     
     @IBAction func tapConfirmButton(_ sender: Any) {
         if nickNameTF.text == "" {
-            showAlert()
+            showAlert(title: "닉네임을 정해주세요!")
         }else {
+            checkOverlap(completion:{ overlap in
+                if overlap {
+                    self.showAlert(title: "닉네임이 중복되었습니다.")
+                    return
+                }
+            })
             loadingLabel.isHidden = false
             UserDefaults.standard.setValue(nickNameTF.text!, forKey: "nickname")
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
